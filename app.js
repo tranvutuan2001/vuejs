8325f9b93ef6ecaa061abaceba04c324945ed24d
HieuLe <hieu.le@rwth-aachen.de>
2021-11-06 23:24:59 +0100

    ba-bar + anh qdog

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 new Vue({
-    el: "#app",
-    data: {
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
+  el: "#app",
+  data: {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  },
+  methods: {
+    countDownTimer() {
+      const countDownDate = new Date("Nov 27, 2021 00:00:00").getTime();
+      setInterval(() => {
+        const now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        const distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        this.hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      }, 1000);
     },
-    methods: {
-        countDownTimer() {
-            const countDownDate = new Date("Nov 27, 2021 00:00:00").getTime();
-            setInterval(() => {
-                const now = new Date().getTime();
+  },
 
-                // Find the distance between now and the count down date
-                const distance = countDownDate - now;
-
-                // Time calculations for days, hours, minutes and seconds
-                this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            }, 1000)
-
-        }
-    },
-    created: function () {
-        this.countDownTimer()
-    }
+  created: function () {
+    this.countDownTimer();
+  },
 });
---
+++ ba-bar.js
@@ -0,0 +1,21 @@
+const countDownDate = new Date("Nov 27, 2021 00:00:00").getTime();
+const start = new Date("Nov 6, 2021 21:30:00").getTime();
+
+duration = (countDownDate - start) / 1000;
+console.log(duration);
+const now = new Date().getTime();
+const left_org = (countDownDate - now) / 1000;
+
+let width_full = (left_org / duration) * 100;
+
+document.getElementById("hp").style.width = width_full.toString() + "%";
+setInterval(() => {
+  const now1 = new Date().getTime();
+  let left = (countDownDate - now1) / 1000;
+
+  let gone = left_org - left;
+
+  width_full -= (gone / duration) * 100;
+
+  document.getElementById("hp").style.width = width_full.toString() + "%";
+}, 1000);
